1847dddf60fcae181328bc64adfe7d7c42be9ac7
dentedfork2 <diarmaid.roche@gmail.com>
2019-11-11 18:28:46 -0800

    Update GameManager.h

--- ScratchPractice1/GameManager.h
+++ ScratchPractice1/GameManager.h
@@ -3,10 +3,11 @@
 class GameManager
 {
 private:
-	int board[9] = { 0 };
+	// 9 is a majik number
+	int board[9] = { 0 }; // how large is an int?  Does it seem like over kill?
 	bool cellLock[9] = {};
-	int selection;
-	int player;
+	int selection; 
+	int player; // might want unsigned int since player will never be negative?
 	bool valid;
 	bool tieGame;
 
